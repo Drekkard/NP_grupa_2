0765a1ad7f00fb94a43396bdf3dc677108675ed5
Drekkard <73931822+Drekkard@users.noreply.github.com>
2020-12-17 12:46:46 +0100

    Update suma_N_liczb_FilipŁ.c

--- suma_N_liczb_FilipŁ.c
+++ suma_N_liczb_FilipŁ.c
@@ -7,10 +7,13 @@ int main()
     int s=0;
     printf("Podaj ilosc liczb \n");
     scanf("%d",&n);
-    for(int i=0; i<n;i++){
+    
+    for(int i=0; i<n;i++)
+    {
         printf("Podaj liczbe \n");
         scanf("%d",&l);
-        s=s+l;}
+        s=s+l;
+    }
     printf("Suma tych liczb wychodzi %d",s);
     return 0;
 }
